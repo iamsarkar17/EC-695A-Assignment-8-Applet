4353924c825c4bc8b038b6281bbfef0e5cf619d9
Lohit Sarkar <32552122+iamsarkar17@users.noreply.github.com>
2020-05-24 19:43:24 +0530

    Add files via upload

---
+++ Hello.html
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title>My Java Applet</title>
+</head>
+<body>
+<p>Here is my applet:</p>
+<applet width=10 height=100 code="HelloWorld.class"> </applet>
+</body>
+</html>
---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+import java.applet.Applet;
+import java.awt.*;
+public class HelloWorld extends Applet
+{
+    // init is like main() for an application.
+    // It is invoked automatically when the applet runs
+    public void init()
+    {
+        setBackground( Color.blue );
+    }
+    // The paint method is invoked automatically whenever
+    // the graphics screen needs to be refreshed
+    public void paint( Graphics g)
+    {
+        String s="Hello World";
+        super.paint(g);
+        g.setColor( Color.green );
+        g.drawString(s, 10, 100);
+    }
+}
